f3c0500a4fccb425d6624de9a7e0d1070cb562d3
user <aaa@mail>
2015-09-27 20:30:44 +0300

    path

--- tao1/sites/daoerp/index.py
+++ tao1/sites/daoerp/index.py
@@ -10,8 +10,10 @@ import settings
 sys.path.append( settings.root )
 sys.path.append( os.path.dirname( __file__ ) )
 
-from tao1.core.union import init
-
+try:
+    from tao1.core.union import init
+except:
+    from core.union import init
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete( init( loop ) )
