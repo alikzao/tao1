4be9dc1f226bd91a35342f5dad4914f159dfa7ea
user <aaa@mail>
2015-09-27 23:31:28 +0300

    123

--- tao1/apps/app/routes.py
+++ tao1/apps/app/routes.py
@@ -1,8 +1,8 @@
 
-from apps.app.view import  *
-from core.union import route
-
-route( '/lib_mod',       test_lib_mod,		'GET' )
+# from apps.app.view import  *
+# from core.union import route
+#
+# route( '/lib_mod',       test_lib_mod,		'GET' )
 
 
 
