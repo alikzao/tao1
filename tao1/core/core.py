ff25fae96a0d245b0e503e46e61c5361fe32483f
user <aaa@mail>
2015-09-27 21:47:56 +0300

    123

--- tao1/core/core.py
+++ tao1/core/core.py
@@ -5,7 +5,7 @@ from pymongo import *
 import settings
 
 def die(mess):
-	raise Exception(str(mess))
+    raise Exception(str(mess))
 
 # @asyncio.coroutine
 # def server_connect():
@@ -25,17 +25,17 @@ def die(mess):
 
 @asyncio.coroutine
 def get_settings(name, def_val=None):
-	if name in settings.__dict__: return settings.__dict__[name]
-	else: return def_val
+    if name in settings.__dict__: return settings.__dict__[name]
+    else: return def_val
 
 
 @asyncio.coroutine
 def locale_date(format, dt, loc = 'en_US.UTF-8'):
-	""" dt - кортеж """
-	import locale
-	lc = locale.getdefaultlocale()
-	# lc = locale.getlocale(locale.LC_ALL)
-	locale.setlocale(locale.LC_ALL, loc)
-	res = time.strftime(format, dt)
-	locale.setlocale(locale.LC_ALL, lc)
-	return res
+    """ dt - кортеж """
+    import locale
+    lc = locale.getdefaultlocale()
+    # lc = locale.getlocale(locale.LC_ALL)
+    locale.setlocale(locale.LC_ALL, loc)
+    res = time.strftime(format, dt)
+    locale.setlocale(locale.LC_ALL, lc)
+    return res
