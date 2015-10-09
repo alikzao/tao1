4ea52914bc73eb791a7ee34003983b32a51f1cb2
stalker <stalker@soluwell.com>
2015-10-10 01:06:02 +0300

    CHANGED refactored game loop

--- tao1/libs/game/game.py
+++ tao1/libs/game/game.py
@@ -19,181 +19,210 @@ def game(request):
     return templ('libs.game:game')
 
 
-socket = None
-players=[]
-# clients={}
-clients=set()
-max_id = 0
-
 class Player():
     def __init__(self, startX, startY):
         self.id = None
-        self.x = startX; self.y = startY; self.z = 0
-        self.a = 0; self.b = 0
-    def getX(self):
-        return self.x
-    def getY(self):
-        return self.y
-    def getZ(self):
-        return self.z
+        self._x = startX; self._y = startY; self._z = 0
+        self._a = 0; self._b = 0
 
-    def setX(self, newX):
-        self.x = newX
-    def setY(self, newY):
-        self.y = newY
-    def setZ(self, newZ):
-        self.z = newZ
+    def set_pos(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+    def set_rot(self, a, b):
+        self.a, self.b = a, b
+
+    def getX(self):
+        return self._x
+
+    def getY(self):
+        return self._y
+
+    def getZ(self):
+        return self._z
 
     def getA(self):
-        return self.a
+        return self._a
+
     def getB(self):
-        return self.b
+        return self._b
+
+    def setX(self, newX):
+        self._x = newX
+
+    def setY(self, newY):
+        self._y = newY
+
+    def setZ(self, newZ):
+        self._z = newZ
+
     def setA(self, newA):
-        self.a = newA
+        self._a = newA
+
     def setB(self, newB):
-        self.b = newB
+        self._b = newB
+
+    x = property(getX, setX)
+    y = property(getY, setY)
+    z = property(getZ, setZ)
+    a = property(getA, setA)
+    b = property(getB, setB)
+
+
+socket = None
+players = set()
+clients = set()
+last_id = 0
+
+
+def send_all(mess, except_=tuple()):
+    mess = json.dumps(mess)
+    # print('mess', mess)
+    for client in clients:
+        if client not in except_:
+            client.send_str(mess)
+
+
+def h_new(me, e):
+    global last_id
+    assert not hasattr(me, 'player'), id(me)
+
+    me.player = Player(e['x'], e['y'])
+    me.player.client = me
+    me.player.id = last_id
+    last_id += 1
+
+    mess = {'e': "new", 'id': me.player.id, 'x': me.player.x, 'y': me.player.y, 'z': me.player.z, 'msg': 'newPlayer'}
+    send_all(mess, except_=(me,))
+
+    for player in players:  # // Send existing players to the new player
+        mess = {'e': "new", 'id': player.id, 'x': player.x, 'y': player.y, 'z': player.z, 'msg': 'existingPlayer'}
+        me.send_str(json.dumps(mess))
+
+    players.add(me.player)
+
+
+def h_move(me, e):
+    assert hasattr(me, 'player'), id(me)
+    me.player.set_pos(e['x'], e['y'], e['z'])
+
+    mess = {'e': "move", 'id': me.player.id, 'x': me.player.x, 'y': me.player.y, 'z': me.player.z, 'msg': 'move'}
+    send_all(mess, except_=(me,))
+
+
+def h_rotate(me, e):
+    assert hasattr(me, 'player'), id(me)
+    me.player.set_rot(e['a'], e['b'])
+
+    mess = {'e': "rotate", 'id': me.player.id, 'a': me.player.a, 'b': me.player.b, 'msg': 'rotate'}
+    send_all(mess, except_=(me,))
+
+
+def h_shoot(me, e):
+    assert hasattr(me, 'player'), id(me)
+    me.player.set_rot(e['a'], e['b'])
+
+    mess = {'e': "shoot", 'id': me.player.id, 'pos': e['pos'], 'dir': e['dir'], 'd2': e['d2'],
+            'msg': '#{} says: pif-paf'.format(me.player.id)}
+    send_all(mess, except_=(me,))
+
+
+def h_chat(me, e):
+    assert hasattr(me, 'player'), id(me)
+    me.player.set_rot(e['a'], e['b'])
+
+    mess = {'e': "chat", 'id': me.player.id, 'mes': e['mes']}
+    send_all(mess, except_=(me,))
+
+
+def close(me):
+    assert hasattr(me, 'player'), id(me)
+    print('serv onclose; id', str(me.id))
+
+    yield from me.close()
+    clean(me)
+
+
+def clean(me):
+    if hasattr(me, 'player'):
+        players.remove(me.player)
+    else:
+        print("onClientDisconnect   no player found", str(me.id))
+
+    clients.remove(me)
+
+    print('close players => ', players)
+    print('close clients => ', clients)
+
+    mess = {'e': "remove", id: me.player.id, 'msg': 'remove'}
+    send_all(mess)
+
+
+def handle_game(me, e):
+    e = json.loads(e)
+    action = e['e']
+    if action in debug_handlers:
+        print('Requested action:', action)
+        # print ( '======================>>>>>>>>>>>>>>>', e )
+    if action in handlers:
+        handler = handlers[action]
+        handler(me, e)
+    else:
+        print('Unknown action:', e['e'], '===>>>', e)
+        # send_all(e)
+
+handlers = {
+    'new': h_new,
+    'move': h_move,
+    'rotate': h_rotate,
+    'shoot': h_shoot,
+    'chat': h_chat,
+}
+
+# show all except 'move'
+debug_handlers = set(handlers.keys()) - {'move'}
 
 
 @asyncio.coroutine
 def game_handler(request):
+    # init connection
     ws = web.WebSocketResponse()
     ws.start(request)
     clients.add(ws)
+
+    # infinite message loop
     while True:
         msg = yield from ws.receive()
         # print('yield from ', msg)
-        if msg.tp == MsgType.text:
-            # e = json.loads( msg.data )
-            if msg.data == 'close':
-                yield from ws.close()
-                print('serv onclose '+e, 'max_id',str(ws.id))
-                removePlayer = playerById(ws.id)
-                if not removePlayer: print("onClientDisconnect   Player not found: "+str(ws.id))
-                print('close players => ', players)
-                print('close clients => ', clients)
-
-                # delete clients[ws.id];
-                # players.splice(players.indexOf(removePlayer), 1);
-
-
-                mess = json.dumps( {'e':"remove", id: max_id, 'msg':'remove'} )
-                for client in clients:
-                    try:
-                        client.send_str(mess)
-                    except(e):
-                        print('# error exception => # '+e)
+        try:
+            if msg.tp == MsgType.text:
+                if msg.data == 'close':
+                    print('client requests close')
+                    close(ws)
+                else:
+                    handle_game(ws, msg.data)
+            elif msg.tp == aiohttp.MsgType.close:
+                print('websocket connection closed')
+                clean(ws)
+                # break
+            elif msg.tp == aiohttp.MsgType.error:
+                print('ws connection closed with exception %s', ws.exception())
+                clean(ws)
+                # break
             else:
-                try:
-                    #     ws.send_str(msg.data + '/answer')
-                    e = json.loads( msg.data )
-                    # print ( '======================>>>>>>>>>>>>>>>', e )
-                    if e['e'] == 'new':
-                        print(' :: if e.e == "new":', e,  id(ws) )
-                        newPlayer = Player(e['x'], e['y'])
-                        newPlayer.id = max_id
-                        # player = newPlayer
-                        player = newPlayer
-                        ws.player = player
+                print('unknow websocket message type:', msg.tp, id(ws))
 
-                        mess = json.dumps( {'e':"new", 'id':newPlayer.id, 'x':newPlayer.getX(), 'y':newPlayer.getY(),'z':newPlayer.getZ(), 'msg':'newPlayer'} )
-                        print( 'mess', mess)
-                        for client in clients:
-                            print( 'client =>', client )
-                            # if client.player.id != player.id:
-                            if hasattr(client, 'player') and client.player.id != player.id:
-                                client.send_str(mess)
+        except Exception as e:
+            print('Dark forces tried to break down our infinite loop', e)
+            traceback.print_tb(e.__traceback__)
 
-                        # //for(var i in clients) clients[i].send(mess);
-                        for i in range(0, len(players)):  #// Send existing players to the new player
-                            existingPlayer = players[i]
-                            mess = json.dumps( {'e':"new", 'id': existingPlayer.id, 'x':existingPlayer.getX(), 'y':existingPlayer.getY(),'z':existingPlayer.getZ(), 'msg':'existingPlayer'} )
-                            ws.send_str( mess)
-
-                        players.append(newPlayer)
-                    elif e['e'] == 'move':
-                        # print(' :: if e.e == "move":', e,  id(ws) )
-                        movePlayer = player
-                        if not movePlayer:
-                            print("__onMovePlayer__ Player not found: "+ str(max_id))
-                            return
-                        movePlayer.setX(e['x'])
-                        movePlayer.setY(e['y'])
-                        movePlayer.setZ(e['z'])
-                        # //console.log('Player '+max_id+' move has joined', 'id', movePlayer.id, 'x', movePlayer.getX(), 'y',movePlayer.getY());
-                        mess = json.dumps( {'e':"move", 'id':movePlayer.id, 'x':movePlayer.getX(), 'y':movePlayer.getY(),'z':movePlayer.getZ(), 'msg':'move'} )
-                        print('move clients', clients)
-                        for client in clients:
-                            print('===clients->', player.id)
-                            # if client.player.id != player.id:
-                            if hasattr(client, 'player') and client.player.id != player.id:
-
-                                    # //clients[i].send(mess);
-                                # if clients[i].readyState != OPEN: print('Client state is ' + clients[i].readyState)
-                                # else:
-                                client.send_str(mess)
-                    elif e['e'] == 'rotate':
-                        print(' :: if e.e == "rot":', e,  id(ws) )
-                        rotPlayer = player
-                        if not rotPlayer:
-                            print("__onRotatePlayer__ Player not found: "+str(max_id))
-                            return
-                        rotPlayer.setA(e['a'])
-                        rotPlayer.setB(e['b'])
-                        # //console.log('Player '+max_id+' move has joined', 'id', movePlayer.id, 'x', movePlayer.getX(), 'y',movePlayer.getY());
-                        mess = json.dumps( {'e':"rotate", 'id':rotPlayer.id, 'a':rotPlayer.getA(), 'b':rotPlayer.getB(), 'msg':'move'} )
-                        for client in clients:
-                            # //if (clients[i].player.id != this.player.id)
-                            # if client.player.id != player.id:
-                            if hasattr(client, 'player') and client.player.id != player.id:
-
-                                client.send_str(mess)
-                    elif e['e'] == 'shoot':
-                        print(' :: if e.e == "shot":', e,  id(ws) )
-                        sPlayer = player
-                        ws.player = player
-                        if not sPlayer:
-                            print("__onShootPlayer__ Player not found: "+str(max_id))
-                            return
-                        print('Player '+str(max_id)+' shoot has joined', 'id', sPlayer.id, 'x', sPlayer.getX(), 'y',sPlayer.getY())
-                        mess = json.dumps( {'e':"shoot", 'id':sPlayer.id, 'pos':e['pos'], 'dir':e['dir'], 'd2':e['d2']} )
-                        for client in clients:
-                            # if client.player.id != player.id:
-                            if hasattr(client, 'player') and client.player.id != player.id:
-
-                                 client.send_str(mess)
-                                 print('shoot request=====================================')
-                    elif e['e'] == 'chat':
-                        print(' :: if e.e == "chat":', e,  id(ws) )
-                        chatPlayer = player
-                        # //if (!chatPlayer) { console.log("__onChatPlayer__ Player not found: "+max_id); return; }
-                        print('Player '+str(max_id)+' chat has joined', 'id', chatPlayer.id, 'mess', e['mes'])
-                        mess = json.dumps( {'e':"chat", 'id':chatPlayer.id, 'mes':e['mes']} )
-                        for client in clients:
-                            # if client.player.id != player.id:
-                            if hasattr(client, 'player') and client.player.id != player.id:
-
-                                client.send_str(mess)
-                    else:
-                        print('получено сообщение ' + e)
-                        for client in clients:
-                            client.send_str(e)
-                except Exception as e:
-                    print('EXCEPTION', e)
-                    traceback.print_tb(e.__traceback__)
-        elif msg.tp == aiohttp.MsgType.close:
-            print('websocket connection closed')
-            # break
-        elif msg.tp == aiohttp.MsgType.error:
-            print('ws connection closed with exception %s', ws.exception())
-            # break
     return ws
 
 
-def playerById(id):
-#    console.log('players', players);
-    for i in range(0, players):
-        if players[i].id == id: return players[i];
-    return False
+# def playerById(id):
+# #    console.log('players', players);
+#     for i in range(0, players):
+#         if players[i].id == id: return players[i];
+#     return False
 
 
 
