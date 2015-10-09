27039be5b30781a6f33aec15a408d429cd644327
user <alikzao@gmail.com>
2015-10-09 23:22:42 +0300

    change

--- tao1/sites/daoerp/apps/app/routes.py
+++ tao1/sites/daoerp/apps/app/routes.py
@@ -2,10 +2,10 @@ from apps.app.view import  *
 from core.union import route
 
 
-route( '/',         page,				'GET', 'page' )
-route( '/db',       test_db,			'GET', 'test_db' )
+route( '/',         page,				'GET', 'page'       )
+route( '/db',       test_db,			'GET', 'test_db'    )
 
-route( '/ws',       ws,					'GET', 'ws' )
+route( '/ws',       ws,					'GET', 'ws'         )
 route( '/wsh',      ws_handler,			'GET', 'ws_handler' )
 
 
