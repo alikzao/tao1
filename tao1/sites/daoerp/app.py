3e75aa09d0bf6e3fc9e81fdb164f8777e73fc59d
user <alikzao@gmail.com>
2015-10-14 04:51:51 +0300

    fix gunicorn  run file

--- tao1/sites/daoerp/app.py
+++ tao1/sites/daoerp/app.py
@@ -5,11 +5,14 @@ import sys, os
 assert sys.version >= '3.3', 'Please use Python 3.4 or higher.'
 
 
-import settings
-sys.path.append(settings.root)
-sys.path.append( os.path.dirname( __file__ ) )
+sys.path.append(os.path.dirname(__file__))
 
-from tao1.core.union import init_gunicorn
+import settings
+
+sys.path.append(settings.tao_path)
+
+
+from core.union import init_gunicorn
 app = init_gunicorn()
 
 # gunicorn app:app -k aiohttp.worker.GunicornWebWorker -b localhost:6677
