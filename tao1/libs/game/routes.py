02aeb5e6aa940948e04fe83de031f6151978f278
user <alikzao@gmail.com>
2016-02-16 09:13:17 +0200

    remove old files

--- tao1/libs/game/routes.py
+++ tao1/libs/game/routes.py
@@ -2,12 +2,12 @@ from libs.game.game import  *
 from core.union import route
 
 
-route('GET',  	'/pregame',	        pregame,	    'pregame'    )
-route('GET',  	'/game',		    game,		    'game'       )
-route('POST', 	'/check_room',	    check_room,	    'check_room' )
-route('GET',  	'/babylon',	        babylon,	    'babylon'    )
-route('GET',  	'/game_handler',	game_handler,   'g_h'        )
-route('GET',  	'/test/mesh',	    test_mesh,	    'test_mesh'  )
+route('GET',  	'/pregame',		pregame,		'pregame'   )
+route('GET',  	'/game',		game,			'game'      )
+route('POST', 	'/check_room',	check_room,		'check_room')
+route('GET',  	'/babylon',		babylon,		'babylon'   )
+route('GET',  	'/game_handler',game_handler,	'g_h'       )
+route('GET',  	'/test/mesh',	test_mesh,		'test_mesh' )
 
 
 
