102aac07b96b2fc31a252d13606e4022d80e22d3
user <alikzao@gmail.com>
2016-02-16 09:46:05 +0200

    change

--- setup.py
+++ setup.py
@@ -8,14 +8,13 @@ if sys.version_info < (3, 4, 1):
     raise RuntimeError("tao1 requires Python 3.4.1+")
 
 setup(name='tao1',
-      version="0.2.0",
+      version="0.2.2",
       description=("framework, CMS and CRM for aiohttp"),
 #      lond_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
 
       scripts=['tao1/core/utils.py'],
       classifiers=[
           'Intended Audience :: Developers',
-          'Development Status :: alfa',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3.4',
           'Topic :: Internet :: WWW/HTTP'],
