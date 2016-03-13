4f6c18a304c79dbfb65bf32190c89d6c9b4d84d5
user <alikzao@gmail.com>
2016-03-14 01:50:07 +0200

    change

--- tao1/libs/chat/static/chat.js
+++ tao1/libs/chat/static/chat.js
@@ -5,17 +5,14 @@
         var wsUri = (window.location.protocol == 'https:' && 'wss://' || 'ws://') + window.location.hostname + ':80/online';
         ws = new WebSocket(wsUri);
         ws.onopen = function () {
-            //console.log('users', users);
             ws.send(JSON.stringify({'e':"new", 'users':users} ) );
         };
         ws.onmessage = function (event) {
             var msg = JSON.parse(event.data);
             console.warn('msg', msg);
             if (msg.e == 'on') {
-                console.log('if==================');
                 var us = msg.users;
                 for(var res in us) {
-                    console.log('res', us[res]);
                     $('[user_id="' + us[res] + '"]').find('.fa-circle').css('color', '#00a300');
                 }
             } else if (msg.e == 'move') {
