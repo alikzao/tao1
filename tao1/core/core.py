3fc703d0ff3addf1469ac8267da3f805a5f222e4
user <alikzao@gmail.com>
2016-03-17 03:08:09 +0200

    change

--- tao1/core/core.py
+++ tao1/core/core.py
@@ -203,6 +203,7 @@ def send_mail(app):
     s = smtplib.SMTP()
     s.connect('localhost') # connect to the SMTP server
     while True:
+        print('cron_while')
         doc = app.db.queue.mail.find_one({"sending":{"$exists":False}})
         if not doc: break
         doc['sending']= True
